f799bcc5e0b7b5f74c2493796233e9ac78c56504
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-07-04 01:58:23 +0600

    footer added to root

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+
 import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer';
 
 const Root = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
