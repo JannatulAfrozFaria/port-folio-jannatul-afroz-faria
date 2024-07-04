0a032801748c6c313b81beec87f496993fda0793
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-07-04 17:14:43 +0600

    used tech added in first project

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -27,6 +27,23 @@ const Projects = () => {
                             <li> <span className="text-gray-300 font-medium ">Asset Request System: </span>Allows Employees to browse available assets and submit requests for the required assets.</li>
                             <li> <span className="text-gray-300 font-medium ">Comprehensive Dashboard: </span>Offers Employees a personal dashboard to track their requests and HR Managers an administrative dashboard to manage asset inventory and request approvals efficiently.</li>
                         </ul>
+                        {/* Technology */}
+                        <p className="text-lg text-yellow-500 font-medium">Used Technology :</p>
+                        <div>
+                            <div className='grid grid-cols-3 gap-4'>
+                                <img className='w-16' src="https://i.ibb.co/Rb3zwyz/html1.png" alt="HTML" />
+                                <img className='w-16' src="https://i.ibb.co/g37V1Gd/css4.png" alt="CSS" />
+                                <img className='w-16' src="https://i.ibb.co/q12wPdr/js.png" alt="JS" />
+                            </div>
+                            <div className='grid grid-cols-3 gap-4'>
+                                <img className='w-16' src="https://i.ibb.co/RPXxDxG/tailwind.png" alt="Tailwind" />
+                                <img className='w-16' src="https://i.ibb.co/jrjDYLq/react.png" alt="React" />
+                                <img className='w-16' src="https://i.ibb.co/DMxdFmm/firebase.png" alt="Firebase" />
+                                <img className='w-16' src="https://i.ibb.co/pbLRSVY/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="MonoDB" />
+                                <img className='w-16 h-16' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
+                                <img className='w-16' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 {/* Project---2 */}
