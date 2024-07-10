32d4a877f4c886a1065ded05e1b15000a39bf6d2
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-07-11 02:50:00 +0600

    used technology section updated

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -30,7 +30,7 @@ const Projects = () => {
                         {/* Technology----- */}
                         <p className="text-lg text-yellow-500 font-medium">Used Technology :</p>
                         <div>
-                            <div className='grid grid-cols-9 gap-2 w-1/2'>
+                            <div className='grid grid-cols-9 gap-2 w-1/2 items-center'>
                                 <img className='w-12' src="https://i.ibb.co/Rb3zwyz/html1.png" alt="HTML" />
                                 <img className='w-12' src="https://i.ibb.co/g37V1Gd/css4.png" alt="CSS" />
                                 <img className='w-12' src="https://i.ibb.co/q12wPdr/js.png" alt="JS" />
@@ -38,7 +38,7 @@ const Projects = () => {
                                 <img className='w-12' src="https://i.ibb.co/jrjDYLq/react.png" alt="React" />
                                 <img className='w-12' src="https://i.ibb.co/DMxdFmm/firebase.png" alt="Firebase" />
                                 <img className='w-12' src="https://i.ibb.co/pbLRSVY/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="MonoDB" />
-                                <img className='w-12 h-16' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
+                                <img className='w-12' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
                                 <img className='w-12' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
                             </div>
                         </div>
@@ -75,7 +75,7 @@ const Projects = () => {
                         {/* Technology----- */}
                         <p className="text-lg text-yellow-500 font-medium">Used Technology :</p>
                         <div>
-                            <div className='grid grid-cols-9 gap-2 w-1/2'>
+                            <div className='grid grid-cols-9 gap-2 w-1/2 items-center'>
                                 <img className='w-12' src="https://i.ibb.co/Rb3zwyz/html1.png" alt="HTML" />
                                 <img className='w-12' src="https://i.ibb.co/g37V1Gd/css4.png" alt="CSS" />
                                 <img className='w-12' src="https://i.ibb.co/q12wPdr/js.png" alt="JS" />
@@ -83,7 +83,7 @@ const Projects = () => {
                                 <img className='w-12' src="https://i.ibb.co/jrjDYLq/react.png" alt="React" />
                                 <img className='w-12' src="https://i.ibb.co/DMxdFmm/firebase.png" alt="Firebase" />
                                 <img className='w-12' src="https://i.ibb.co/pbLRSVY/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="MonoDB" />
-                                <img className='w-12 h-16' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
+                                <img className='w-12' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
                                 <img className='w-12' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
                             </div>
                         </div>
@@ -120,7 +120,7 @@ const Projects = () => {
                         {/* Technology----- */}
                         <p className="text-lg text-yellow-500 font-medium">Used Technology :</p>
                         <div>
-                            <div className='grid grid-cols-9 gap-2 w-1/2'>
+                            <div className='grid grid-cols-9 gap-2 w-1/2 items-center'>
                                 <img className='w-12' src="https://i.ibb.co/Rb3zwyz/html1.png" alt="HTML" />
                                 <img className='w-12' src="https://i.ibb.co/g37V1Gd/css4.png" alt="CSS" />
                                 <img className='w-12' src="https://i.ibb.co/q12wPdr/js.png" alt="JS" />
@@ -128,7 +128,7 @@ const Projects = () => {
                                 <img className='w-12' src="https://i.ibb.co/jrjDYLq/react.png" alt="React" />
                                 <img className='w-12' src="https://i.ibb.co/DMxdFmm/firebase.png" alt="Firebase" />
                                 <img className='w-12' src="https://i.ibb.co/pbLRSVY/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="MonoDB" />
-                                <img className='w-12 h-16' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
+                                <img className='w-12' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
                                 <img className='w-12' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
                             </div>
                         </div>
