77bad951fc52c3e682224e03545dd767926778be
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-12 05:55:26 +0600

    tech div replaced with component

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -78,17 +78,7 @@ const Projects = () => {
                         {/* Technology----- */}
                         <p className="text-lg text-yellow-500 font-medium">Used Technology :</p>
                         <div>
-                            <div className='grid grid-cols-9 gap-2 w-1/2 items-center'>
-                                <img className='w-12' src="https://i.ibb.co/Rb3zwyz/html1.png" alt="HTML" />
-                                <img className='w-12' src="https://i.ibb.co/g37V1Gd/css4.png" alt="CSS" />
-                                <img className='w-12' src="https://i.ibb.co/q12wPdr/js.png" alt="JS" />
-                                <img className='w-12' src="https://i.ibb.co/RPXxDxG/tailwind.png" alt="Tailwind" />
-                                <img className='w-12' src="https://i.ibb.co/jrjDYLq/react.png" alt="React" />
-                                <img className='w-12' src="https://i.ibb.co/DMxdFmm/firebase.png" alt="Firebase" />
-                                <img className='w-12' src="https://i.ibb.co/pbLRSVY/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="MonoDB" />
-                                <img className='w-12' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
-                                <img className='w-12' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
-                            </div>
+                            <UsedTechnology></UsedTechnology>
                         </div>
                         {/* Project Links------ */}
                         <p className="text-lg text-yellow-500 font-medium">Project Links :</p>
