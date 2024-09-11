4b31be8a1a53e1634c78164d4b7eb883f98919c3
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-12 01:39:35 +0600

    text-style modified

--- src/Pages/CV.jsx
+++ src/Pages/CV.jsx
@@ -40,15 +40,15 @@ const CV = () => {
             <div className=" grid grid-cols-1 md:grid-cols-2 gap-6 mb-8 w-3/4 mx-auto">
                 <div className="shadow-lg shadow-yellow-600 card-body card-bordered rounded-3xl" >
                     <p className="text-yellow-600 card-title">Extra Curricular</p>
-                    <p><span className="text-yellow-600 font-medium" >Title:</span> Smart Book Writer</p>
-                    <p><span className="text-yellow-600 font-medium" >Organization: </span>10 Minute School</p>
-                    <p> <span className="text-yellow-600 font-medium" >Timeline: </span> January 2019 – December 2019 </p>
+                    <p><span className="text-yellow-600 font-medium" >Title :</span> Smart Book Writer</p>
+                    <p><span className="text-yellow-600 font-medium" >Organization : </span>10 Minute School</p>
+                    <p> <span className="text-yellow-600 font-medium" >Timeline : </span> January 2019 – December 2019 </p>
                 </div>
                 <div className="shadow-lg shadow-yellow-600 card-body card-bordered rounded-3xl">
                     <p className="text-yellow-600 card-title ">Achievements & Awards
-                    <p><span className="text-yellow-600 font-medium" >Organization: </span>Biswa Shahitta Kendro</p>		 	  
+                    <p><span className="text-yellow-600 font-medium" >Organization : </span>Biswa Shahitta Kendro</p>		 	  
                     </p>
-                    <p> <span className="text-yellow-600 font-medium" >Timeline: </span> June, 2011</p>
+                    <p> <span className="text-yellow-600 font-medium" >Timeline : </span> June, 2011</p>
                     <p> <span className="text-yellow-600 font-medium" >First Prize | </span> Writing Competition</p>
                     <p> <span className="text-yellow-600 font-medium" >Winning Prize : </span>Visiting <span className="green-text">2</span> countries of Asia : <span className="green-text">Nepal & India</span>.</p>
                 </div>
