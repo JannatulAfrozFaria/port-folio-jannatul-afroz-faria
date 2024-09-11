da02784d358482b259f71e82fc262f4815e7a150
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-12 01:47:52 +0600

    title modified

--- src/Pages/CV.jsx
+++ src/Pages/CV.jsx
@@ -47,9 +47,8 @@ const CV = () => {
                     <p> <span className="text-yellow-600 font-medium" >Timeline : </span> January 2019 – December 2019 </p>
                 </div>
                 <div className="shadow-lg shadow-green-700 card-body card-bordered rounded-3xl">
-                    <p className="text-yellow-600 card-title ">Achievements & Awards
-                    <p></p>		 	  
-                    </p>
+                    <p className="text-yellow-600 card-title ">Achievements & Awards</p>
+                    <p><span className="text-yellow-600 font-medium" >Organization : </span>Biswa Shahitta Kendro</p>
                     <p> <span className="text-yellow-600 font-medium" >First Prize | </span> Writing Competition</p>
                     <p> <span className="text-yellow-600 font-medium" >Winning Prize : </span>Visiting <span className="green-text">2</span> countries of Asia : <span className="green-text">Nepal & India</span>.</p>
                     <p> <span className="text-yellow-600 font-medium" >Timeline : </span> June, 2011</p>
