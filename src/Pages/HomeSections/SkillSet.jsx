139e38de4baab64f76e491cb53bfe670e45052a9
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-14 03:55:35 +0600

    skills layout modified

--- src/Pages/HomeSections/SkillSet.jsx
+++ src/Pages/HomeSections/SkillSet.jsx
@@ -9,19 +9,7 @@ const SkillSet = () => {
             <Title heading={'skillset'}></Title>
             <div className=" img_bg3 my-8 p-8 md:py-24 md:px-12 shadow-xl rounded-2xl ">
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-20 w-5/6 mx-auto">
-                    {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-14 col-span-3">
-                        <div>
-                            <p className="text-orange-300 text-lg font-semibold mb-6">Programming Languages</p>
-                            <div className='grid grid-cols-3 gap-4 w-3/4'>
-                                <img className='w-16' src="https://i.ibb.co/Rb3zwyz/html1.png" alt="HTML" />
-                                <img className='w-16' src="https://i.ibb.co/g37V1Gd/css4.png" alt="CSS" />
-                                <img className='w-16' src="https://i.ibb.co/q12wPdr/js.png" alt="JS" />
-                            </div>
-                        </div>
-                        <div>
-                            <Technologies></Technologies>
-                        </div>
-                    </div> */}
+                {/* LANGUAGES---------- */}
                     <div>
                         <p className="text-orange-300 text-lg font-semibold mb-6">Programming Languages</p>
                         <div className='grid grid-cols-3 gap-4 w-3/4'>
@@ -34,6 +22,7 @@ const SkillSet = () => {
                     <div>
                         <Technologies></Technologies>
                     </div>
+                    {/* SOFT-----SKILLS----- */}
                     <div className='text-left text-orange-200  text- md:text-base'>
                         <p className='text-orange-300 text-lg font-semibold mb-6'>As A Web Developer, I'm . . . </p>
                         <div className="flex gap-12 md:gap-8 items-baseline">
