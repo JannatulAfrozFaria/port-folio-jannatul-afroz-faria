19604c4b1b557927b7e5359f2f3f9463f408931e
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-09-14 03:58:35 +0600

    margin updated in technologies section

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -8,9 +8,9 @@ const Technologies = () => {
                  <img className='w-16 rounded-lg' src="https://i.ibb.co/RPXxDxG/tailwind.png" alt="Tailwind" />
                 <img className='w-16 rounded-lg' src="https://i.ibb.co/jrjDYLq/react.png" alt="React" />
                 <img className='w-16 rounded-lg' src="https://i.ibb.co/DMxdFmm/firebase.png" alt="Firebase" />
-                <img className='w-16 rounded-lg' src="https://i.postimg.cc/XJNfVxGs/node-js.png" alt="Vercel" />
-                <img className='w-16 h-16 rounded-lg' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
-                <img className='w-16 rounded-lg' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
+                <img className='w-16 rounded-lg my-2' src="https://i.postimg.cc/XJNfVxGs/node-js.png" alt="Vercel" />
+                <img className='w-16 h-16 rounded-lg my-2' src="https://i.ibb.co/MG8xR7v/express.jpg" alt="Express" />
+                <img className='w-16 rounded-lg my-2' src="https://i.ibb.co/vJ9Cq7r/vercel.jpg" alt="Vercel" />
                
                 <img className='w-16 rounded-lg' src="https://i.ibb.co/pbLRSVY/mongodb-logo-D13-D67-C930-seeklogo-com.png" alt="MonoDB" />
                 <img className='w-16 rounded-lg' src="https://i.postimg.cc/ryJz3xZG/next-white.jpg" alt="Vercel" />
