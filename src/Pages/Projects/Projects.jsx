9f8c1d77704405d642f8e90c2892232b062d9f11
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-06 01:40:11 +0600

    Plenora added

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -24,6 +24,41 @@ const Projects = () => {
                         alt="Album" />
                     </figure>
                     <div className="card-body text-left">
+                        <h2 className="card-title text-yellow-500 font-semibold">Project Name: Plenora Architects</h2>
+                        <p className="text-lg text-yellow-500 font-medium">Description :</p>
+                        <p>This is an asset management website where HR managers can manage, add, update and delete assets. Employees can create asset requests and HR managers can approve or reject those requests.</p>
+                        <p className="text-lg font-medium text-yellow-500 ">Core Feature :</p>
+                        <ul>
+                            <li> <span className="text-gray-400 font-medium ">User Role Management: </span>Differentiates access controls and permissions for two user groups: Employees and HR Managers.</li>
+                            <li> <span className="text-gray-400 font-medium ">Asset Request System: </span>Allows Employees to browse available assets and submit requests for the required assets.</li>
+                            <li> <span className="text-gray-400 font-medium ">Comprehensive Dashboard: </span>Offers Employees a personal dashboard to track their requests and HR Managers an administrative dashboard to manage asset inventory and request approvals efficiently.</li>
+                        </ul>
+                        {/* Technology----- */}
+                        <p className="text-lg text-yellow-500 font-medium">Used Technology :</p>
+                        <div>
+                            <UsedTechnology></UsedTechnology>
+                        </div>
+                        {/* Project Links------ */}
+                        <p className="text-lg text-yellow-500 font-medium">Project Links :</p>
+                        <div>
+                            <p> <span className="text-gray-400 font-medium ">Live Link : </span> <a href="https://asset-track-pro-34487.web.app/">
+                            <span className="text-green-400">Asset Track Pro Live Website</span>
+                            </a></p>
+                            <p> <span className="text-gray-400 font-medium ">GitHub Link (client-side) : </span> <a href="https://github.com/JannatulAfrozFaria/asset-track-pro-client">
+                            <span className="text-green-400">Asset Track Pro Client Side GitHub Link</span></a></p>
+                            <p> <span className="text-gray-400 font-medium ">GitHub Link (server-side) : </span> <a href="https://github.com/JannatulAfrozFaria/asset-track-pro-server">
+                            <span className="text-green-400">Asset Track Pro Server Side Github Link</span></a> </p>
+                        </div>
+                    </div>
+                </div>
+                 {/* Project---2 */}
+                <div data-aos="zoom-in" data-aos-delay="1000" data-aos-duration="3000" className="card lg:card-side bg-base-100 shadow-xl mb-6">
+                    <figure>
+                        <img className="w-[700px] h-full"
+                        src="https://i.ibb.co/MCty8YK/notebook.png"
+                        alt="Album" />
+                    </figure>
+                    <div className="card-body text-left">
                         <h2 className="card-title text-yellow-500 font-semibold">Project Name: Asset Track Pro</h2>
                         <p className="text-lg text-yellow-500 font-medium">Description :</p>
                         <p>This is an asset management website where HR managers can manage, add, update and delete assets. Employees can create asset requests and HR managers can approve or reject those requests.</p>
@@ -51,7 +86,7 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
-                {/* Project---2 */}
+                {/* Project---3 */}
                 <div data-aos="flip-up" data-aos-delay="1500" data-aos-duration="3000" className="card lg:card-side bg-base-100 shadow-xl mb-6">
                     <figure>
                         <img className="w-[700px] h-full"
@@ -86,7 +121,7 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
-                {/* Project---3 */}
+                {/* Project---4 */}
                 <div data-aos="zoom-in" data-aos-delay="500" data-aos-duration="3000" className="card lg:card-side bg-base-100 shadow-xl mb-6">
                     <figure>
                         <img className="w-[700px] h-full"
@@ -121,7 +156,7 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
-                {/* Project---4 */}
+                {/* Project---5 */}
                 <div data-aos="flip-up" data-aos-delay="200" data-aos-duration="3000" className="card lg:card-side bg-base-100 shadow-xl mb-6 md:mb-12">
                     <figure>
                         <img className="w-[700px] h-full"
